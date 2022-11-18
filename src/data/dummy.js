60d6dcf56c97e15eb580db63cfe41a2c15279b38
aziz-codes <azizisoffline@gmail.com>
2022-11-18 18:37:45 +0500

    basic layout of app and data items

---
+++ src/data/dummy.js
@@ -0,0 +1,39 @@
+import burger from "./images/burger.jpeg";
+import friedMutton from "./images/fried-mutton.jpeg";
+import kfcPack from "./images/kfcpack.jpeg";
+import rice from "./images/rice.jpeg";
+import salad from "./images/salad.jpeg";
+import spagetty from "./images/spagetty.png";
+
+export const items = [
+  {
+    name: "Burger",
+    image: burger,
+    price: 250,
+  },
+  {
+    name: "Fried Mutton",
+    image: friedMutton,
+    price: 930,
+  },
+  {
+    name: "KFC Pack",
+    image: kfcPack,
+    price: 1205,
+  },
+  {
+    name: "Rice",
+    image: rice,
+    price: 180,
+  },
+  {
+    name: "Russian Salad",
+    image: salad,
+    price: 130,
+  },
+  {
+    name: "Spagetty",
+    image: spagetty,
+    price: 340,
+  },
+];
