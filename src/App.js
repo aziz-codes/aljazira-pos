c3f7073b9406aa5e87a47be02c5a4cf3245c1c53
aziz-codes <azizisoffline@gmail.com>
2022-11-19 18:03:56 +0500

    Searching and filtering on products

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React from "react";
 import Items from "./components/Items";
 import Navbar from "./components/Navbar";
 import Table from "./components/Table";
-import { ItemContext } from "./context/ItemsContext";
 
 const App = () => {
-  const { list } = ItemContext();
-  console.log(list);
   return (
     <div className="flex flex-col h-screen bg-white">
       <Navbar />
 
       <div className="flex flex-col md:flex-row  mt-14 w-full">
-        <div className="w-1/2 max-h-[500px] overflow-y-auto scrollbar border">
+        <div className="md:w-1/2 w-full max-h-[500px] overflow-y-auto scrollbar border">
           <Table />
         </div>
-        <div className="flex flex-wrap  w-1/2 bg-green  max-h-[500px] overflow-y-auto scrollbar border gap-4 justify-center pt-2">
+
+        <div
+          className="flex flex-wrap w-full md:w-1/2 bg-green  max-h-[500px] overflow-y-auto scrollbar
+         border gap-4 justify-center pt-2"
+        >
           <Items />
         </div>
       </div>
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { items } from "../data/dummy";
 import { ItemContext } from "../context/ItemsContext";
 const Items = () => {
   const { list, setList } = ItemContext();
+  const [products, setProducts] = useState(items);
   const handleClickItem = (item) => {
     if (list.includes(item)) {
       let index = list.findIndex((index) => index.name === item.name);
@@ -14,33 +15,53 @@ const Items = () => {
       setList([...list, item]);
     }
   };
+
+  const handleChange = (e) => {
+    const filtered = items.filter((item) =>
+      item.name.startsWith(e.toUpperCase() || e.toUpperCase())
+    );
+    setProducts(filtered);
+  };
+
   return (
-    <>
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className="flex flex-col shadow-lg bg-white hover:scale-95 transition-all 
-     duration-100 ease-in-out w-44 h-44  rounded-xl cursor-pointer p-2"
-          onClick={() => {
-            handleClickItem(item);
+    <div className="flex flex-col">
+      <div className="ml-4">
+        <input
+          type="text"
+          placeholder="Search for Items"
+          className="h-8 rounded-sm w-1/2 p-2 outline-none border border-sky-400 transition-shadow"
+          onKeyUp={(e) => {
+            handleChange(e.target.value);
           }}
-        >
-          <img
-            src={item.image}
-            className="h-28 max-h-24 rounded-lg"
-            alt={item.name}
-          />
-          <div className="text-center pt-2">
-            <h4 className="font-medium text-lg overflow-hidden whitespace-nowrap text-ellipsis">
-              {item.name}
-            </h4>
-            <span className="text-sm font-bold tracking-tight">
-              {item.price} PKR
-            </span>
+        />
+      </div>
+      <div className="flex flex-wrap items-center justify-center">
+        {products.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col shadow-lg bg-white hover:scale-95 transition-all 
+     duration-100 ease-in-out w-44 h-44  rounded-xl cursor-pointer p-2"
+            onClick={() => {
+              handleClickItem(item);
+            }}
+          >
+            <img
+              src={item.image}
+              className="h-28 max-h-24 rounded-lg"
+              alt={item.name}
+            />
+            <div className="text-center pt-2">
+              <h4 className="font-medium text-lg overflow-hidden whitespace-nowrap text-ellipsis">
+                {item.name}
+              </h4>
+              <span className="text-sm font-bold tracking-tight">
+                {item.price} PKR
+              </span>
+            </div>
           </div>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
+    </div>
   );
 };
 
