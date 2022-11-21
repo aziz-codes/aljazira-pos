1650ceecf5dd8437bb149d5cf0f29494bdbc4fec
aziz-codes <azizisoffline@gmail.com>
2022-11-21 23:04:01 +0500

    connection status

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { WifiIcon } from "@heroicons/react/24/outline";
 const Navbar = () => {
   const [isOnline, setOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    window.addEventListener("online", () => {
+      setOnline(true);
+    });
+    window.addEventListener("offline", () => {
+      setOnline(false);
+    });
+  }, []);
+
   return (
     <div
       className="fixed top-0 z-50 w-full h-10 overflow-hidden flex flex-row
      justify-between items-center shadow-lg bg-white"
     >
-      <h1> you are {isOnline ? "Online" : "Offline"}</h1>
+      <div className="mx-2">
+        {isOnline ? (
+          <WifiIcon className="h-5 w-5 text-green-500" title="connected" />
+        ) : (
+          <WifiIcon className="h-5 w-5 text-red-500" title="not connected" />
+        )}
+      </div>
     </div>
   );
 };
