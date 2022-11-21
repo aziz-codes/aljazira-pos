e7842b0f0b359f5498ac2e2a3ab7cf8b26877970
aziz-codes <azizisoffline@gmail.com>
2022-11-21 22:43:30 +0500

    sticky table head

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 const Navbar = () => {
+  const [isOnline, setOnline] = useState(navigator.onLine);
   return (
     <div
       className="fixed top-0 z-50 w-full h-10 overflow-hidden flex flex-row
      justify-between items-center shadow-lg bg-white"
     >
-      Navbar components
+      <h1> you are {isOnline ? "Online" : "Offline"}</h1>
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,27 +13,31 @@ const Table = () => {
 
   return (
     <div className="w-full m-2 border relative p-4 divide-y-4">
-      <table className="w-full">
-        <thead>
-          <tr className="border">
-            <td>S.No</td>
-            <td>Name</td>
-            <td>Price</td>
-            <td>Qty</td>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((item, index) => (
-            <tr key={index} className="m-2">
-              <td>{index}</td>
-              <td>{item.name}</td>
-              <td>{item.price}</td>
-              <td>{item.quantity}</td>
+      <div
+        className="max-h-[450px] overflow-y-auto scrollbar-track-slate-400 scrollbar-thin scrollbar-corner-rounded
+       scrollbar-thumb-slate-100"
+      >
+        <table className="w-full">
+          <thead className="sticky -top-1 bg-white z-50 h-10 w-full">
+            <tr className="border">
+              <td>S.No</td>
+              <td>Name</td>
+              <td>Price</td>
+              <td>Qty</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-
+          </thead>
+          <tbody>
+            {list.map((item, index) => (
+              <tr key={index} className="m-2">
+                <td>{index}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       <div className="flex flex-row p-2 pt-2 w-full justify-between h-14 shadow-xl items-center">
         <h2 className="font-bold tracking-tight">Total : {perItemTotal}</h2>
         <h4>
